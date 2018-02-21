0fb376ade2226829942b0e8c61ac30cc9c409439
Trevor Seitz <trevinla@gmail.com>
2018-02-21 21:16:39 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -24,6 +24,6 @@ function divSelector() {
 
 function firstListItem() {
   // var firstKid = $('ul:first-child.#pic-list');
-  var firstKid = $("ul.piclist :first-child"); 
+  var firstKid = $("ul.pic-list :first-child"); 
   return firstKid;
 }
